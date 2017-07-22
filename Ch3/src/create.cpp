afb7ca1f9a937e05484a40dea04235581635cd87
Carter Hay <clccmh@gmail.com>
2017-07-21 22:21:06 -0500

    converted ch3 to use stb_image_write

--- Ch3/src/create.cpp
+++ Ch3/src/create.cpp
@@ -1,7 +1,10 @@
-#include <stdlib.h>
 #include <iostream>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include "vec3.h"
 #include "ray.h"
+
+using namespace std;
 
 // I need to grok this. I have no idea what it does.
 vec3 color(const ray& r) {
@@ -12,20 +15,35 @@ vec3 color(const ray& r) {
   return (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
-int main() {
-  int width = 200;
-  int height = 100;
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    cout << "Incorrect number of arguments\nUsage: run.out output_file.png" << endl;
+    return 1;
+  }
 
-  std::cout << "P3\n" << width << " " << height << "\n255\n";
-  
-  for (float g = 0; g < height; g++) {
-    for (float r = 0; r < width; r++) {
+  const unsigned int width = 200;
+  const unsigned int height = 100;
+  const unsigned char channels = 3;
+  unsigned int index;
+
+  unsigned char* data = new unsigned char[width * height * channels];
+
+  for (unsigned int y = 0; y < height; y++) {
+    for (unsigned int x = 0; x < width; x++) {
+
+      index = (y * width * channels) + (x * channels);
       
-      vec3 col(float(r) / float(width), float(g) / float(height), 0.0);
-      std::cout << int(col.x*255) << " " << int(col.y*255) << " " << col.z 
-        << std::endl;
+      vec3 col(float(x) / float(width), float(y) / float(height), 0.0);
+
+      data[index]     = col.x * 255; // Red channel
+      data[index + 1] = col.y * 255; // Green channel
+      data[index + 2] = 0; // Blue channel
     }
   }
+
+  stbi_write_png(argv[1], width, height, channels, data, width * channels);
+
+  delete[] data;
 
   return 0;
 }
